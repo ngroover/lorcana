d0bb1548f37dbde68e88a22f8ff9c4481963b4a1
Nathan Groover <nathan.groover@gmail.com>
2024-03-14 00:36:06 -0400

    Start adding unittests for drawing the starting hand.

---
+++ test_starting_hand.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import unittest
+from contestant import Contestant
+from decklists import amber_amethyst,sapphire_steel,olaf
+from controller import RandomController,Controller
+from game import Game,GamePhase,PlayerTurn
+from action import FirstPlayerAction,PassAction,DrawAction
+from test_support import test_contestants
+
+class TestStartingHand(unittest.TestCase):
+    def test_draw_first_player(self):
+        c = test_contestants()
+        game = Game(c[0],c[1],RandomController('env'))
+        game.phase = GamePhase.DRAW_STARTING_HAND
+
+        actions = game.get_actions()
+        self.assertEqual(29, len(actions))
+        self.assertTrue(all(type(x) is DrawAction for x in actions))
+        # chance to draw olaf is 3 out of 60
+        olaf_draw_action = DrawAction(olaf,3)
+        self.assertTrue(olaf_draw_action in actions)
+
+    def test_draw_card_first_player(self):
+        c = test_contestants()
+        game = Game(c[0],c[1],RandomController('env'))
+        game.phase = GamePhase.DRAW_STARTING_HAND
+
+        # draw an olaf
+        olaf_draw_action = DrawAction(olaf,3)
+        game.process_action(olaf_draw_action)
+
+        # still 29 cards we can draw, but 1 less olaf
+        actions = game.get_actions()
+        self.assertEqual(29, len(actions))
+        self.assertEqual(sum(1 for _ in 
+                filter(lambda x: x.card == olaf,actions)),1)    #ensure only 1 olaf draw action
+        # we can only draw 2 olafs now
+        olaf_draw_action = DrawAction(olaf,2)
+        self.assertTrue(olaf_draw_action in actions)
+        # our hand size is bigger
+        self.assertTrue(len(self.p1.hand), 1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()

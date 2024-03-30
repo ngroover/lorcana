90b5cec471679d6a402cb1d2ade583435a5fb919
Nathan Groover <nathan.groover@gmail.com>
2024-03-30 15:42:15 -0400

    Add tests for testing when duplicates characters are on board. In some situations the choices will actually be different.

---
+++ game_generator.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+from contestant import Contestant
+from decklists import amber_amethyst,sapphire_steel
+from controller import RandomController,Controller
+from game import Game,GamePhase,PlayerTurn
+from decklists import olaf,pascal,captain_hook,aurora,maleficent,fire_the_cannons,dinglehopper
+from action import FirstPlayerAction,DrawAction,PassAction,InkAction,PlayCardAction,QuestAction,TriggeredAbilityAction,AbilityTargetAction,ChallengeAction,ChallengeTargetAction
+from ability import HealingTriggeredAbility
+
+class GameGenerator:
+    def test_contestants(self):
+        c1 = Contestant(amber_amethyst, RandomController('test1'))
+        c2 = Contestant(sapphire_steel, RandomController('test2'))
+        return [c1,c2]
+
+    def init_game(self):
+        c = self.test_contestants()
+        self.game = Game(c[0],c[1],RandomController('env'))
+        return self
+
+    def draw_opening_hand(self):
+        first_player_swap=False
+        p1_hand=[olaf,olaf,olaf,pascal,pascal,dinglehopper,dinglehopper]
+        p2_hand=[captain_hook,captain_hook,captain_hook, aurora, maleficent, maleficent,maleficent]
+
+        self.game.process_action(FirstPlayerAction(first_player_swap))
+
+        if first_player_swap:
+            all_cards=p2_hand+p1_hand
+        else:
+            all_cards=p1_hand+p2_hand
+        for x in all_cards:
+            self.game.process_action(DrawAction(x))
+
+        return self
+
+    def pass_mulligan(self):
+        self.game.process_action(PassAction())
+        self.game.process_action(PassAction())
+        return self
+
+    def ink_pascal(self):
+        self.game.process_action(InkAction(pascal))
+        return self
+
+    def ink_hook(self):
+        self.game.process_action(InkAction(captain_hook))
+        return self
+
+    def quest_hook(self):
+        self.game.process_action(QuestAction(captain_hook))
+        return self
+
+    def play_hook(self):
+        self.game.process_action(PlayCardAction(captain_hook))
+        return self
+
+    def use_dinglehopper(self):
+        self.game.process_action(TriggeredAbilityAction(HealingTriggeredAbility(),dinglehopper))
+        return self
+
+    def olaf_challenge_hook(self):
+        self.game.process_action(ChallengeAction(olaf))
+        self.game.process_action(ChallengeTargetAction(captain_hook))
+        return self
+
+    def hook_challenge_olaf(self):
+        self.game.process_action(ChallengeAction(captain_hook))
+        self.game.process_action(ChallengeTargetAction(olaf))
+        return self
+
+    def play_olaf(self):
+        self.game.process_action(PlayCardAction(olaf))
+        return self
+
+    def ink_olaf(self):
+        self.game.process_action(InkAction(olaf))
+        return self
+
+    def quest_olaf(self):
+        self.game.process_action(QuestAction(olaf))
+        return self
+
+    def quest_hook(self):
+        self.game.process_action(QuestAction(captain_hook))
+        return self
+
+    def play_dinglehopper(self):
+        self.game.process_action(PlayCardAction(dinglehopper))
+        return self
+
+    def target_olaf(self):
+        self.game.process_action(AbilityTargetAction(olaf, PlayerTurn.PLAYER1))
+        return self
+
+    def target_hook(self):
+        self.game.process_action(AbilityTargetAction(captain_hook, PlayerTurn.PLAYER2))
+        return self
+
+
+    def pass_turn(self):
+        self.game.process_action(PassAction())
+        cards_to_draw = self.game.get_actions()
+        # draw whatever card. doesn't matter
+        self.game.process_action(cards_to_draw[0])
+        return self
+
+
+
---
+++ test_duplicates.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import unittest
+from game_generator import GameGenerator
+from action import PlayCardAction,TriggeredAbilityAction,AbilityTargetAction,QuestAction
+from ability import HealingTriggeredAbility
+from decklists import dinglehopper,olaf
+from game import GamePhase
+
+class TestDuplicates(unittest.TestCase):
+    def test_different_quester_choices(self):
+        g = GameGenerator()
+        
+        g.init_game().draw_opening_hand().pass_mulligan()\
+                .ink_pascal().play_olaf().pass_turn()\
+                .ink_hook().play_hook().pass_turn()\
+                .ink_olaf().play_olaf().quest_olaf().pass_turn()\
+                .hook_challenge_olaf().pass_turn()
+        #NOTE: once we add challenger this test will break because captain hook
+        # kills olaf
+
+        # one olaf has damage one doesn't
+        self.assertEqual(1, g.game.p1.in_play_characters[0].damage)
+        self.assertEqual(0, g.game.p1.in_play_characters[1].damage)
+
+        actions = g.game.get_actions()
+
+        print(f'actions {actions}')
+        self.assertEqual(2, len(list(
+                filter(lambda x: type(x) is QuestAction and \
+                        x.card == olaf, actions))))
+    def test_same_quester_choices(self):
+        g = GameGenerator()
+        
+        g.init_game().draw_opening_hand().pass_mulligan()\
+                .ink_pascal().play_olaf().pass_turn()\
+                .ink_hook().play_hook().pass_turn()\
+                .ink_olaf().play_olaf().quest_olaf().pass_turn()\
+                .pass_turn()
+        #NOTE: once we add challenger this test will break because captain hook
+        # kills olaf
+
+        # both olafs don't have damage
+        self.assertEqual(0, g.game.p1.in_play_characters[0].damage)
+        self.assertEqual(0, g.game.p1.in_play_characters[1].damage)
+
+        actions = g.game.get_actions()
+
+        print(f'actions {actions}')
+        self.assertEqual(1, len(list(
+                filter(lambda x: type(x) is QuestAction and \
+                        x.card == olaf, actions))))
+
+
+    def test_quest_damaged_olaf(self):
+        g = GameGenerator()
+        
+        g.init_game().draw_opening_hand().pass_mulligan()\
+                .ink_pascal().play_olaf().pass_turn()\
+                .ink_hook().play_hook().pass_turn()\
+                .ink_olaf().play_olaf().quest_olaf().pass_turn()
+
+    def test_quest_damaged_olaf(self):
+        g = GameGenerator()
+        
+        g.init_game().draw_opening_hand().pass_mulligan()\
+                .ink_pascal().play_olaf().pass_turn()\
+                .ink_hook().play_hook().pass_turn()\
+                .ink_olaf().play_olaf().quest_olaf().pass_turn()
+
+
+
+if __name__ == '__main__':
+    unittest.main()

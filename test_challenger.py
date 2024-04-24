ed128a88b10275349e9b56436ca99fdd6b4bbf41
Nathan Groover <nathan.groover@gmail.com>
2024-04-24 15:32:16 -0400

    Forgot to submit test_challenger

---
+++ test_challenger.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import unittest
+from game_generator import GameGenerator
+from action import PlayCardAction,TriggeredAbilityAction,AbilityTargetAction,QuestAction,ChallengeAction,ChallengeTargetAction
+from ability import HealingTriggeredAbility
+from decklists import dinglehopper,olaf,captain_hook,amber_amethyst, sapphire_steel
+from game import GamePhase
+
+class TestChallenger(unittest.TestCase):
+    def test_captain_hook_challenger(self):
+        g = GameGenerator()
+        
+        g.init_game(amber_amethyst, sapphire_steel)\
+                .setup_cards([olaf],[captain_hook])\
+                .quest(olaf).pass_turn()
+
+        self.assertEqual(1, len(g.game.p1.in_play_characters))
+
+        g.hook_challenge_olaf().pass_turn()
+
+        #olaf got banished so we should have no characters in play
+        self.assertEqual(0, len(g.game.p1.in_play_characters))
+
+if __name__ == '__main__':
+    unittest.main()
